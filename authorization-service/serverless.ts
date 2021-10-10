febe5fb993dd1893b7295e345d7d21cda495e5c4
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-10-10 14:21:08 +0300

    feat: add authorization service with basicAuthorizer lambda

---
+++ authorization-service/serverless.ts
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv';
+import type { AWS } from '@serverless/typescript';
+
+import basicAuthorizer from '@functions/basicAuthorizer';
+
+dotenv.config();
+const { femiarkh } = process.env;
+
+const serverlessConfiguration: AWS = {
+  service: 'authorization-service',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack', 'serverless-dotenv-plugin'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      femiarkh,
+    },
+    lambdaHashingVersion: '20201221',
+    stage: 'dev',
+    region: 'eu-west-1',
+  },
+  // import the function via paths
+  functions: { basicAuthorizer },
+};
+
+module.exports = serverlessConfiguration;
